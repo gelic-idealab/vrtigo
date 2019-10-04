4d7f22c381ada0ac7e59fd8e51c31004495a2b16
Wallace <rwwalla2@illinois.edu>
2019-10-04 16:40:06 -0500

    redirect back to form with preview & download options

--- app.py
+++ app.py
@@ -28,83 +28,76 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-@app.route('/<string:session_id>', methods=['GET'])
-def session_preview(session_id):
-    
-    return app.send_static_file(session_id+'/index.html', session_id=session_id)
-
 @app.route("/", methods=['GET', 'POST'])
 def main():
     print(request.method)
     if request.method == 'POST':
         try:
-            print("I am in post request")
+            # get tour params from form
             grid_row = int(request.form['grid_row'])
             grid_column = int(request.form['grid_column'])
-
             title = request.form['title']
             email = request.form['email']
             session_id = ''
             message = ''
 
-            if request.form['submit_button'] == 'Preview':
+            if request.form['submit_button'] == 'Generate Tour':
                 grid_location = request.files['grid_location']
-                message, session_id = aft.generate_package_web_tour(grid_location, title, grid_row, grid_column, 'static')
-                print('returned value = {} {}'.format(message, session_id))
-                if os.path.exists(os.path.join(os.getcwd(), SESSION_TRACKER)):
-                    with open(os.path.join(os.getcwd(), SESSION_TRACKER), 'r+') as json_file:
-                        session = json.load(json_file)
-                        print(session)
-                        session[str(session_id)] = {}
-                        session[str(session_id)]['created_time'] = str(
-                            datetime.fromtimestamp(os.path.getctime(os.path.join('static', str(session_id)))))
-                        json.dump(session, json_file)
-                        json_file.seek(0)
-                        json.dump(session, json_file)
-                        json_file.truncate()
-                    # json_file.close()
+                # use aframtour library to generate package: returns success/fail message, image extension value, and a session id
+                message, img_ext, session_id = aft.generate_package_web_tour(grid_location, title, grid_row, grid_column, 'static')
+                print('generate_package_web_tour returned:', message, img_ext, session_id) 
+                
+                # if no error message, set tour_generated flag, redirect back to form page with pre-loaded params
+                if message == '':
 
+                    # log session to session_tracker.json, create file if necessary
+                    if os.path.exists(os.path.join(os.getcwd(), SESSION_TRACKER)):
+                        with open(os.path.join(os.getcwd(), SESSION_TRACKER), 'r+') as json_file:
+                            session = json.load(json_file)
+                            session[str(session_id)] = {}
+                            session[str(session_id)]['created_time'] = str(
+                                datetime.fromtimestamp(os.path.getctime(os.path.join('static', str(session_id)))))
+                            json.dump(session, json_file)
+                            json_file.seek(0)
+                            json.dump(session, json_file)
+                            json_file.truncate()
+                    else:
+                        with open(SESSION_TRACKER, 'w+') as json_file:
+                            session = {}
+                            session[str(session_id)] = {}
+                            session[str(session_id)]['created_time'] = str(datetime.fromtimestamp(os.path.getctime(os.path.join('static', str(session_id)))))
+                            json.dump(session, json_file)
+
+                    tour_generated = True
+                    return render_template(
+                        'WebVR_BuildingTour_FE.html', 
+                        tour_generated=tour_generated, 
+                        session_id=session_id,
+                        title=title,
+                        email=email,
+                        grid_row=grid_row, 
+                        grid_column=grid_column)
+                
+                # if error message, return error message to render on form page
                 else:
-                    with open(SESSION_TRACKER, 'w+') as json_file:
-                        session = {}
-                        session[str(session_id)] = {}
-                        session[str(session_id)]['created_time'] = str(datetime.fromtimestamp(os.path.getctime(os.path.join('static', str(session_id)))))
-                        json.dump(session, json_file)
-                    # json_file.close()
+                    raise Exception(message)
 
-                # if message == '':
-                    # print(os.path.join(os.getcwd(), 'static', str(session_id), 'index.html'))
-                return redirect("/"+str(session_id))
-                    # # return render_template('index2.html', numberOfRows=grid_row, numberOfCol=grid_column,
-                    #                        path=os.path.join('static', str(session_id), 'static', 'images'),
-                    #                        image_type=str('jpg'),
-                    #                        title=title, email=email, is_file_ready='No', file_location='')
-                # else:
-                #     return render_template('Success_Page.html', category='danger',
-                #                            html_to_display='<h1>'+message+'</h1>',
-                #                            )
-            elif request.form['submit_button'] == 'Download':
-                grid_location = request.form['grid_location']
-                filename = grid_location
-                session_id = filename.split('\\')[1]
-                return render_template('index2.html', numberOfRows=grid_row, numberOfCol=grid_column,
-                                        path=grid_location, image_type=str('jpg'),
-                                        title=title, email=email,
-                                        is_file_ready='Yes', file_location=os.path.join('static', str(session_id)+'.zip')
-                                        )
         except Exception as err:
             logging.error(str(err))
-            print('Error = ', str(err))
-            return None
+            error = str(err)
+            return render_template(
+                'WebVR_BuildingTour_FE.html', 
+                error=error,
+                tour_generated=False, 
+                session_id=session_id,
+                title=title,
+                email=email,
+                grid_row=grid_row, 
+                grid_column=grid_column)
 
+    # if request method is not POST
     else:
         return render_template('WebVR_BuildingTour_FE.html')
-
-
-
-@app.route("/web_vr", methods=['GET', 'POST'])
-def web_vr_page():
-    return render_template('WebVR_BuildingTour_FE.html')
 
 
 def before_request():
