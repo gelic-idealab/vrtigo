09e9510c94b90c42387643a49ed5318f3f21b37b
gdharra2 <46692795+gdharra2@users.noreply.github.com>
2019-11-12 16:47:48 -0600

    Updated scheduling time and time frame for files to be picked from cleanup sessions file

--- clean_up_sessions.py
+++ clean_up_sessions.py
@@ -11,20 +11,20 @@ def delete_session_files():
         with open(os.path.join(os.getcwd(), SESSION_TRACKER), 'r+') as json_file:
             session = json.load(json_file)
             for each_session in session:
-                if datetime.now() - timedelta(hours=2) < parser.parse(session[each_session]['created_time']) < datetime.now() - timedelta(hours=1):
+                if os.path.exists(os.path.join(os.getcwd(), 'static', each_session)) and parser.parse(session[each_session]['created_time']) < datetime.now() - timedelta(hours=1):
                     files_to_be_deleted.append(each_session)
 
-        json_file.close()
 
     print(os.listdir(os.path.join(os.getcwd(),'static')))
 
     for each_file in os.listdir(os.path.join(os.getcwd(),'static')):
         if each_file in files_to_be_deleted:
             shutil.rmtree(os.path.join(os.getcwd(),'static',each_file))
-        if each_file.replace('.zip', '') in files_to_be_deleted:
-            os.remove(os.path.join(os.getcwd(),'static', each_file))
+            if os.path.exists(os.path.join(os.getcwd(), 'static', each_file+'.zip')):
+                os.remove(os.path.join(os.getcwd(),'static', each_file+'.zip'))
 
 
 scheduler = BlockingScheduler()
-scheduler.add_job(delete_session_files, 'interval', hours=1)
+scheduler.add_job(delete_session_files, 'interval', minutes=1)
 scheduler.start()
+
